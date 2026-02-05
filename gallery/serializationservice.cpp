230aa083745ab0714b045de9094f82cfac978706
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-02-05 10:28:39 +0100

    Création de la classe serializationService

---
+++ gallery/serializationservice.cpp
@@ -0,0 +1,12 @@
+#include "serializationservice.h"
+
+serializationservice::serializationservice() {}
+
+void serializationservice::serializeImageModel(ImageModel imageModel) {}
+
+void serializationservice::serializeTabModel(TabModel tabModel) {}
+
+ImageModel serializationservice::deserializeImageModels() {}
+
+TabModel serializationservice::deserializeTabModels() {}
+
