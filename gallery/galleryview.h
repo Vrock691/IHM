431d44b2e1a7c755a2594916c24fbd4733e1cb7c
vanessqufaj9-maker <vanessqufaj9@gmail.com>
2026-02-08 15:16:52 +0100

    Update galleryview.h

--- gallery/galleryview.h
+++ gallery/galleryview.h
@@ -1,14 +1,33 @@
 #ifndef GALLERYVIEW_H
 #define GALLERYVIEW_H
 
+#include <QWidget>
+#include <vector>
+#include <QGridLayout>
 #include "ui_galleryview.h"
+#include "imagemodel.h" 
+#include "tabmodel.h"   
 
 class GalleryView : public QWidget, private Ui::GalleryVue
 {
     Q_OBJECT
 
 public:
-    explicit GalleryView(QWidget *parent = nullptr);
+    explicit GalleryView(std::vector<ImageModel> images, 
+                         std::vector<TabModel> tabs, 
+                         QWidget *parent = nullptr);
+
+    void refreshModel();
+
+private:
+    void openTab(int tabId);
+
+    // Stockage des données
+    std::vector<ImageModel> _allImages;
+    std::vector<TabModel> _tabs;
+    
+    //organise les ImageCell dans le widget galleryGrid
+    QGridLayout* _gridLayout;
 };
 
 #endif // GALLERYVIEW_H
