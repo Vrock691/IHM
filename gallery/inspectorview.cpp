c44744e761f2ee7da9a5ae4d5ab7598180592fe0
SokhnaDIOPD22011515 <smdbdiop@gmail.com>
2026-02-08 14:55:53 +0100

    Ajout de seSelected dans InspectorView et completion de la vue (style à faire)

--- gallery/inspectorview.cpp
+++ gallery/inspectorview.cpp
@@ -1,22 +1,90 @@
 #include "inspectorview.h"
 
 InspectorView::InspectorView(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+    ui(new Ui::InspectorView)
 {
-    setupUi(this);
-    iconImage->setPixmap(
+    ui->setupUi(this);
+
+    ui->iconImage->setPixmap(
         QPixmap(":/icons/image-icon.png")
-            .scaled(24, 24, Qt::KeepAspectRatio, Qt::SmoothTransformation)
+            .scaled(45, 45, Qt::KeepAspectRatio, Qt::SmoothTransformation)
         );
 
-    iconEdit->setPixmap(
-        QPixmap(":/icons/edit-icon.png")
-            .scaled(24, 24, Qt::KeepAspectRatio, Qt::SmoothTransformation)
+    ui->iconEdit->setIcon(
+        QIcon(":/icons/edit-icon.png")
+        );
+    ui->iconEdit->setIconSize(QSize(24, 24));
+
+    ui->iconHeart->setIcon(
+        QIcon(":/icons/heart-icon.png")
+        );
+    ui->iconHeart->setIconSize(QSize(24, 24));
+
+    ui->tabWidget->setTabText(0, "");
+    ui->tabWidget->setTabText(1, "");
+
+    // Onglet 0 : aperçu / infos
+    ui->tabWidget->setTabIcon(
+        0,
+        QIcon(":/icons/eye-icon.png")
         );
 
-    iconHeart->setPixmap(
-        QPixmap(":/icons/heart-icon.png")
+    // Onglet 1 : métadonnées/redimensionnement
+    ui->tabWidget->setTabIcon(
+        1,
+        QIcon(":/icons/redimension-icon.png")
+        );
+
+    // Taille des icônes dans la barre d’onglets
+    ui->tabWidget->setIconSize(QSize(32, 32));
+
+    ui->iconInfo->setPixmap(
+        QPixmap(":/icons/info-icon.png")
             .scaled(24, 24, Qt::KeepAspectRatio, Qt::SmoothTransformation)
         );
 
 }
+
+InspectorView::~InspectorView()
+{
+    delete ui;
+}
+
+void InspectorView::setSelected(ImageModel* imageModel)
+{
+    _selected = imageModel;
+
+    if (!_selected) {
+        ui->labelFileName->setText("Aucune image sélectionnée");
+        ui->labelPath->clear();
+        ui->labelFormat->clear();
+        ui->labelSize->clear();
+        ui->labelDimensions->clear();
+        return;
+    }
+
+    ui->labelFileName->setText(
+        QString::fromStdString(_selected->fileName())
+        );
+
+    ui->labelPath->setText(
+        QString::fromStdString(_selected->path())
+        );
+
+    ui->labelFormat->setText(
+        QString::fromStdString(_selected->format())
+        );
+
+    ui->labelDimensions->setText(
+        QString("%1 x %2")
+            .arg(_selected->width())
+            .arg(_selected->height())
+        );
+
+    double sizeKB = _selected->sizeBytes() / 1024.0;
+    ui->labelSize->setText(
+        QString::number(sizeKB, 'f', 2) + " Ko"
+        );
+}
+
--- gallery/mainwindow.cpp
+++ gallery/mainwindow.cpp
@@ -1,9 +1,29 @@
 #include "mainwindow.h"
+#include "inspectorview.h"
+#include "imagemodel.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
-    setupUi(this);
+    ui->setupUi(this);
+
+    InspectorView* inspector = new InspectorView(this);
+
+    ui->dockInspector->setWidget(inspector);
+
+    // Bloque le dock
+    ui->dockInspector->setFeatures(QDockWidget::NoDockWidgetFeatures);
+
+    // Fixe la taille
+    // ui->dockInspector->setMinimumWidth(250);
+    // ui->dockInspector->setMaximumWidth(250);
+    // ui->dockInspector->setMinimumHeight(400);
+    // ui->dockInspector->setMaximumHeight(400);
+
+    // Test
+    ImageModel* testImage = new ImageModel(":/icons/image-icon.png");
+    inspector->setSelected(testImage);
 }
 
 MainWindow::~MainWindow() {}
