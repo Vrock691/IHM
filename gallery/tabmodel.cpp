157ab810796ad63d20932baee90b7e1578ff651a
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-02-09 08:34:41 +0100

    Ajout du polymorphisme pour les classes IFilter et IOrderer

--- gallery/tabmodel.cpp
+++ gallery/tabmodel.cpp
@@ -1,14 +1,23 @@
 #include "tabmodel.h"
 
-TabModel::TabModel(int index, QString name, std::vector<IFilter> filters, IOrderer orderer) :
-    _index(index), _name(name), _filters(filters), _orderer(orderer) {}
+TabModel::TabModel(
+    int index,
+    QString name,
+    std::vector<std::unique_ptr<IFilter>> filters,
+    std::unique_ptr<IOrderer> orderer
+    ) :
+    _index(index),
+    _name(name),
+    _filters(std::move(filters)),
+    _orderer(std::move(orderer))
+{}
 
-int TabModel::getIndex() const { return this->_index; }
-QString TabModel::getName() const { return this->_name; }
-std::vector<IFilter> TabModel::getFilters() const { return this->_filters; }
-IOrderer TabModel::getOrderer() const { return this->_orderer; }
+int TabModel::getIndex() const { return _index; }
+QString TabModel::getName() const { return _name; }
+const std::vector<std::unique_ptr<IFilter>>& TabModel::getFilters() const { return _filters; }
+IOrderer* TabModel::getOrderer() const { return _orderer.get(); }
 
-void TabModel::setIndex(int index) { this->_index = index; }
-void TabModel::setName(QString name) { this->_name = name; }
-void TabModel::setFilters(const std::vector<IFilter> filters) { this->_filters = filters; }
-void TabModel::setOrderer(const IOrderer orderer) { this->_orderer = orderer; }
+void TabModel::setIndex(int index) { _index = index; }
+void TabModel::setName(QString name) { _name = name; }
+void TabModel::setFilters(std::vector<std::unique_ptr<IFilter>> filters) { _filters = std::move(filters); }
+void TabModel::setOrderer(std::unique_ptr<IOrderer> orderer) { _orderer = std::move(orderer); }
--- gallery/tabmodel.h
+++ gallery/tabmodel.h
@@ -4,26 +4,32 @@
 #include "QString"
 #include "ifilter.h"
 #include "iorderer.h"
+#include <memory>
 
 class TabModel
 {
     int _index;
     QString _name;
-    std::vector<IFilter> _filters;
-    IOrderer* _orderer;
+    std::vector<std::unique_ptr<IFilter>> _filters;
+    std::unique_ptr<IOrderer> _orderer;
 
 public:
-    TabModel(int index, QString name, std::vector<IFilter> filters, IOrderer* orderer);
+    TabModel(int index, QString name, std::vector<std::unique_ptr<IFilter>> filters, std::unique_ptr<IOrderer> orderer);
 
     int getIndex() const;
     QString getName() const;
-    std::vector<IFilter> getFilters() const;
-    IOrderer getOrderer() const;
+    const std::vector<std::unique_ptr<IFilter>>& getFilters() const;
+    IOrderer* getOrderer() const;
 
     void setIndex(int index);
     void setName(QString name);
-    void setFilters(const std::vector<IFilter> filters);
-    void setOrderer(const IOrderer orderer);
+    void setFilters(const std::vector<std::unique_ptr<IFilter>> filters);
+    void setOrderer(const std::unique_ptr<IOrderer> orderer);
+
+    TabModel(const TabModel&) = delete;
+    TabModel& operator=(const TabModel&) = delete;
+    TabModel(TabModel&&) = default;
+    TabModel& operator=(TabModel&&) = default;
 };
 
 #endif // TABMODEL_H
