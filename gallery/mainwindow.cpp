2c0a3ff0020776aff5484b7d16c8712d03942170
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-02-13 16:57:46 +0100

    test: Enlèvement des parties inutiles

--- gallery/mainwindow.cpp
+++ gallery/mainwindow.cpp
@@ -26,30 +26,6 @@ MainWindow::MainWindow(QWidget *parent)
     // Test
     //ImageModel* testImage = new ImageModel(":/icons/image-icon.png");
     //inspector->setSelected(testImage);
-
-    SerializationService service;
-    std::vector<std::string> keywords;
-    keywords.push_back("Key1");
-    keywords.push_back("Key2");
-    ImageModel imageModel = ImageModel(
-        "path2",
-        200,
-        200,
-        "png",
-        "name",
-        500,
-        "28/25/20",
-        "29/05/02",
-        Color::WHITE,
-        "desc",
-        keywords,
-        0,
-        Feeling::HAPPY
-        );
-    service.serializeImageModel(imageModel);
-    qDebug() << "qDebug";
-    std::vector<ImageModel> images = service.deserializeImageModels();
-    qDebug() << "qDebug";
 }
 
 MainWindow::~MainWindow() {}
