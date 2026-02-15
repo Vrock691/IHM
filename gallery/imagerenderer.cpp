928c13a96fe6c279b8f0380f7a37ed398e6ad0d1
Paul Maget <paul.maget@etu.univ-grenoble-alpes.fr>
2026-02-15 20:29:23 +0100

    Ajout de ImageRenderer

---
+++ gallery/imagerenderer.cpp
@@ -0,0 +1,31 @@
+#include "imagerenderer.h"
+#include "imagemodel.h"
+#include "ui_imagerenderer.h"
+
+ImageRenderer::ImageRenderer(ImageModel imageModel, QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::ImageRenderer)
+    , _imageModel(imageModel)
+{
+    ui->setupUi(this);
+
+    QImage image;
+    image.load(QString::fromStdString(_imageModel.path()));
+
+    _pixmap = QPixmap::fromImage(image);
+    _pixmap.scaled(_pixmap.size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+    ui->_label->setPixmap(_pixmap);
+    ui->_label->setMinimumSize(50, 50);
+    ui->_label->setAlignment(Qt::AlignCenter);
+}
+
+ImageRenderer::~ImageRenderer()
+{
+    delete ui;
+}
+
+void ImageRenderer::resizeEvent(QResizeEvent *event)
+{
+    QWidget::resizeEvent(event);
+    ui->_label->setPixmap(_pixmap.scaled(ui->_label->size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation));
+}
---
+++ gallery/imagerenderer.h
@@ -0,0 +1,30 @@
+#ifndef IMAGERENDERER_H
+#define IMAGERENDERER_H
+
+#include "imagemodel.h"
+#include <QWidget>
+
+namespace Ui {
+class ImageRenderer;
+}
+
+class ImageRenderer : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ImageRenderer(ImageModel imageModel, QWidget *parent = nullptr);
+    ~ImageRenderer();
+
+    ImageModel imageModel() { return _imageModel; }
+
+private:
+    Ui::ImageRenderer *ui;
+    ImageModel _imageModel;
+    QPixmap _pixmap;
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+};
+
+#endif // IMAGERENDERER_H
