f1a845838be126399e9667ed555552e825a2548d
vanessqufaj9-maker <vanessqufaj9@gmail.com>
2026-02-19 10:32:15 +0100

    Update imagemodel.cpp with unknown

--- gallery/imagemodel.cpp
+++ gallery/imagemodel.cpp
@@ -153,10 +153,11 @@ ImageModel::ImageModel(const std::string& path)
 
     //propriétés utilisateur
     _score = 0;
-    _feeling = HAPPY; 
-    _mainColor = WHITE; 
+    _feeling = UNKNOWN_FEELING; 
+    _mainColor = UNKNOWN_COLOR; 
     _keyWords = {};
 }
+
 
 
 
